509f9da9ac46f7d7c73e841571662fad8bc47d4e
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-01-07 14:49:46 +0100

    en proceso el Sign Out

--- app/src/main/java/com/exmaple/wildbicycle/WildBicicleApp.kt
+++ app/src/main/java/com/exmaple/wildbicycle/WildBicicleApp.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WildBicicleApp: Application()
+class WildBicicleApp : Application()
--- app/src/main/java/com/exmaple/wildbicycle/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/exmaple/wildbicycle/ui/home/HomeViewModel.kt
@@ -3,16 +3,41 @@ package com.exmaple.wildbicycle.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.exmaple.wildbicycle.managers.UserManager
 import com.exmaple.wildbicycle.utils.Event
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val userManager: UserManager
+) : ViewModel() {
 
 
     private var _navigate = MutableLiveData<Event<Navigate>>()
     val navigate: LiveData<Event<Navigate>> = _navigate
 
+    private var _errorMessage = MutableLiveData<Event<String>>()
+    val errorMessage: LiveData<Event<String>> = _errorMessage
+
+
+    fun signOut() {
+        userManager.signOut { result ->
+            result.fold(
+                onSuccess = {
+                    if (it) _navigate.postValue(Event(Navigate.GoLogin))
+                },
+                onFailure = { error ->
+                    _errorMessage.postValue(
+                        Event(
+                            error.message ?: "Error al intentar iniciar sesion"
+                        )
+                    )
+                }
+            )
+        }
+    }
+
 
     enum class Navigate() {
-        GoNext
+        GoLogin
     }
 }
