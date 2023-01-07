fe39d28a6c2c299fa5e6b8a639af54b6497052e8
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-01-07 21:13:18 +0100

    Funcionando el cerrar sesion he tenido que implementar un boton en el Home porque no hay manera de hacerlo a traves del menu lateral ejecutar la funcion que hace que peuda cerrar sesion una mierda el DrawerLayout d elos huevos

--- app/src/main/java/com/exmaple/wildbicycle/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/exmaple/wildbicycle/ui/home/HomeViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.exmaple.wildbicycle.managers.UserManager
 import com.exmaple.wildbicycle.utils.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class HomeViewModel @Inject constructor(
     private val userManager: UserManager
 ) : ViewModel() {
@@ -18,26 +20,20 @@ class HomeViewModel @Inject constructor(
     private var _errorMessage = MutableLiveData<Event<String>>()
     val errorMessage: LiveData<Event<String>> = _errorMessage
 
-
-    fun signOut() {
+    fun sigingOut() {
         userManager.signOut { result ->
             result.fold(
                 onSuccess = {
-                    if (it) _navigate.postValue(Event(Navigate.GoLogin))
+                    if (it) _navigate.postValue(Event(Navigate.GoNext))
                 },
                 onFailure = { error ->
-                    _errorMessage.postValue(
-                        Event(
-                            error.message ?: "Error al intentar iniciar sesion"
-                        )
-                    )
+                    _errorMessage.postValue(Event(error.message ?: "Error en desLogearte"))
                 }
             )
         }
     }
 
-
     enum class Navigate() {
-        GoLogin
+        GoNext
     }
 }
