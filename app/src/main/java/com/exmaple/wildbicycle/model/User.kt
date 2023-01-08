72775e9d254b6e5e5c5555b0af56f050e58d5d11
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-01-09 00:58:51 +0100

    Cerrar Sesion, Olvidar Contraseña funciona rarisimo en funcionando mas testeo necesita

--- app/src/main/java/com/exmaple/wildbicycle/model/User.kt
+++ app/src/main/java/com/exmaple/wildbicycle/model/User.kt
@@ -5,7 +5,6 @@ import java.util.Date
 data class User(
     val id: String = "",
     val email: String = "",
-    val password: String = "",
     val provider: ProviderType = ProviderType.EMAIL_PASS,
     val date: Date = Date()
 )
