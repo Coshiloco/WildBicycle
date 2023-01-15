a1f9aa1ae54c05dee78074cd9cb2d5ec31c470aa
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-01-15 14:24:21 +0100

    Adding personalize menu refactor

--- app/src/main/java/com/exmaple/wildbicycle/MainActivity.kt
+++ app/src/main/java/com/exmaple/wildbicycle/MainActivity.kt
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.activity_main2_drawer, menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
