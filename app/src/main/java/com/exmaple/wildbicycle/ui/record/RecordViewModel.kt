96f723df68488e837537537b5cc62d38fc21985c
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-01-24 19:45:33 +0100

    CheckToNavigate RecordFragment with HiltModule

--- app/src/main/java/com/exmaple/wildbicycle/ui/record/RecordViewModel.kt
+++ app/src/main/java/com/exmaple/wildbicycle/ui/record/RecordViewModel.kt
@@ -2,9 +2,13 @@ package com.exmaple.wildbicycle.ui.record
 
 import androidx.lifecycle.ViewModel
 import com.exmaple.wildbicycle.bases.BaseViewModel
+import com.exmaple.wildbicycle.managers.UserManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class RecordViewModel : BaseViewModel() {
+class RecordViewModel @Inject constructor(
+    private val userManager: UserManager,
+) : BaseViewModel() {
 
 }
